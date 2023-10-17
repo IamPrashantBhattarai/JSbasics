6f5fcc8875074655f3c32a4a6e7cb71a7a16383c
Prashant <prashantbhattarainepal@gmail.com>
2023-10-17 13:13:39 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//This is my first Java script Program
+console.log("I have found my soulmate as Yunisha Baskota.");
+
+const age = 21;
+// age = 31;
+console.log(age);
+
+let selectedAge = [34, 45, 35];
+selectedAge[4] = "Hello People";
+console.log(selectedAge);
+//In other programming lagnuages the array must contain the one type of items in it
+//But in JS different types data types can be incorporate in a single array.
+
+//Here name is a parameter whereas
+function greet(name, l_name) {
+  console.log("Hello " + name +" "+   l_name);
+}
+//Prashant ia an argument
+greet("Prashant", "Bhattarai");
